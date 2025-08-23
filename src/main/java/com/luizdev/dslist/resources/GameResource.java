0f2799fd68bb350bf37469db6c5a604942a4cfc7
Luiz Felipe <alucardlf1911@gmail.com>
2025-08-23 19:08:58 -0300

    Buscar Dados

--- src/main/java/com/luizdev/dslist/resources/GameResource.java
+++ src/main/java/com/luizdev/dslist/resources/GameResource.java
@@ -1,15 +1,24 @@
 package com.luizdev.dslist.resources;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luizdev.dslist.entities.GameMinDTO;
 import com.luizdev.dslist.service.GameService;
 
-@Controller
-@RequestMapping(value = "/game")
+@RestController
+@RequestMapping(value = "/games")
 public class GameResource {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping
+	public List<GameMinDTO> findAll(){
+		return gameService.findAll();
+	}
 }
--- src/main/java/com/luizdev/dslist/service/GameService.java
+++ src/main/java/com/luizdev/dslist/service/GameService.java
@@ -1,8 +1,12 @@
 package com.luizdev.dslist.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.luizdev.dslist.entities.Game;
+import com.luizdev.dslist.entities.GameMinDTO;
 import com.luizdev.dslist.repositories.GameRepository;
 
 @Service
@@ -10,4 +14,10 @@ public class GameService {
 
 	@Autowired
 	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll(){
+		List<Game> entidades_games = gameRepository.findAll();
+		List<GameMinDTO> entidades = entidades_games.stream().map(x -> new GameMinDTO(x)).toList();		
+		return entidades;
+	}
 }
