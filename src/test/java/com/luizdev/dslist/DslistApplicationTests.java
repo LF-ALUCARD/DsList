3d919624209eee41f6af0e7e260597ad93359365
Luiz Felipe <alucardlf1911@gmail.com>
2025-08-23 16:17:51 -0300

    Criação do Projeto

---
+++ src/test/java/com/luizdev/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizdev.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
